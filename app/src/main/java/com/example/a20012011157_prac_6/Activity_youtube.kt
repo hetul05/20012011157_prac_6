8aed395d8eefb5eb2d3920a8335e38db1f2a69c3
hetul <hetulshah2002@gmail.com>
2022-11-07 10:19:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a20012011157_prac_6/Activity_youtube.kt
@@ -0,0 +1,11 @@
+package com.example.a20012011157_prac_6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_youtube : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+    }
+}
---
+++ app/src/main/java/com/example/a20012011157_prac_6/youtube.kt
@@ -0,0 +1,19 @@
+package com.example.madpractical6_20012021051
+Practical: 6
+20012021051_Raval Nirnay A Page 5
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+class youtube : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+        val youtubeid=""
+        val mywebview=findViewById<WebView>(R.id.wv)
+        val settings=mywebview.settings
+        settings.javaScriptEnabled=true
+        settings.loadWithOverviewMode=true
+        settings.useWideViewPort=true
+        mywebview.loadUrl("https://www.youtube.com/embed/$youtubeid")
+    }
+}
